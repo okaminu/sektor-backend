722dc89072c1bc3ee25fc139a5f821d770775250
Okaminu <aurimasdgt@gmail.com>
2018-01-09 21:06:39 +0200

    TLIST-453: progress

--- src/main/kotlin/lt/tlistas/loginn/backend/Application.kt
+++ src/main/kotlin/lt/tlistas/loginn/backend/Application.kt
@@ -1,8 +1,15 @@
 package lt.tlistas.loginn.backend
 
-class Application {
+import org.springframework.context.support.GenericApplicationContext
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter
+import org.springframework.web.server.adapter.WebHttpHandlerBuilder
+import reactor.ipc.netty.http.server.HttpServer
 
-    fun main(args: Array<String>) {
+fun main(args: Array<String>) {
+    var server = HttpServer.create(8080)
+    var nettyContext = server.start(ReactorHttpHandlerAdapter(
+            WebHttpHandlerBuilder.applicationContext(GenericApplicationContext().apply {
+                refresh()
+            }).build()))
 
-    }
 }
