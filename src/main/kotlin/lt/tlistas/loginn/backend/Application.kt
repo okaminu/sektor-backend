bc7c7fa7b8d3c4bae630b60479ebebd6e81be5af
Okaminu <aurimasdgt@gmail.com>
2018-01-10 17:54:04 +0200

    TLIST-453: simple server

--- src/main/kotlin/lt/tlistas/loginn/backend/Application.kt
+++ src/main/kotlin/lt/tlistas/loginn/backend/Application.kt
@@ -1,15 +1,19 @@
 package lt.tlistas.loginn.backend
 
-import org.springframework.context.support.GenericApplicationContext
 import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter
-import org.springframework.web.server.adapter.WebHttpHandlerBuilder
+import org.springframework.web.reactive.function.BodyInserters.fromObject
+import org.springframework.web.reactive.function.server.HandlerFunction
+import org.springframework.web.reactive.function.server.RequestPredicates
+import org.springframework.web.reactive.function.server.RouterFunctions
+import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.ipc.netty.http.server.HttpServer
+
 
 fun main(args: Array<String>) {
     var server = HttpServer.create(8080)
-    var nettyContext = server.start(ReactorHttpHandlerAdapter(
-            WebHttpHandlerBuilder.applicationContext(GenericApplicationContext().apply {
-                refresh()
-            }).build()))
 
+    val helloWorld = HandlerFunction<ServerResponse> { ServerResponse.ok().body(fromObject("Hello World")) }
+    val helloWorldRoute = RouterFunctions.route(RequestPredicates.path("/hello-world"), helloWorld)
+    RouterFunctions.toHttpHandler(helloWorldRoute)
+    server.startAndAwait(ReactorHttpHandlerAdapter(RouterFunctions.toHttpHandler(helloWorldRoute)))
 }
