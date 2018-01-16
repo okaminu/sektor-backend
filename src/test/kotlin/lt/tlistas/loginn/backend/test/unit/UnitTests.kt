9cab81c9cfa0ca74fba26f16299b3a2feccc7fc8
Okaminu <aurimasdgt@gmail.com>
2018-01-16 21:00:21 +0200

    TLIST-453: current progress on code coverage

---
+++ src/test/kotlin/lt/tlistas/loginn/backend/test/unit/UnitTests.kt
@@ -0,0 +1,12 @@
+package lt.tlistas.loginn.backend.test.unit
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class UnitTests {
+	
+	@Test
+	fun `Some behaviour described as a specification`() {
+		assertTrue(true)
+	}
+}
