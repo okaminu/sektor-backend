8b24089f31711e8e828ea6a8e6d369820069ec39
Okaminu <aurimasdgt@gmail.com>
2018-01-17 17:37:57 +0200

    TLIST-453: handler test

---
+++ src/test/kotlin/lt/tlistas/loginn/backend/test/unit/CollaboratorHandlerTest.kt
@@ -0,0 +1,40 @@
+package lt.tlistas.loginn.backend.test.unit
+
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import lt.tlistas.core.service.UserService
+import lt.tlistas.core.service.WorkLogService
+import lt.tlistas.core.type.entity.Collaborator
+import lt.tlistas.core.type.entity.Company
+import lt.tlistas.core.type.entity.User
+import lt.tlistas.core.type.value_object.TimeRange
+import lt.tlistas.loginn.backend.CollaboratorHandler
+import org.junit.Ignore
+import org.junit.Test
+
+
+class CollaboratorHandlerTest {
+	
+	@Test
+	@Ignore
+	fun `Takes collaborator worktime`() {
+		val userServiceMock : UserService = mock()
+		val workLogServiceMock : WorkLogService= mock()
+
+		val timeRange = TimeRange(0, 1)
+		val user = User().apply {
+			company = Company().apply {
+				addCollaborator(Collaborator()
+						.apply { workTime = timeRange })
+			}
+		}
+		doReturn(user).`when`(userServiceMock).getByEmail(any())
+
+		val collaboratorHandler = CollaboratorHandler(userServiceMock, workLogServiceMock)
+		val response = collaboratorHandler.getWorkTime(mock())
+
+		verify(userServiceMock).getByEmail(any())
+	}
+}
