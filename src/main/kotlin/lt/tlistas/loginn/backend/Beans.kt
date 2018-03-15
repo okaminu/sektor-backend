aae4d9335444fbd7a54dfee67658fbff303d5cb5
Okaminu <aurimasdgt@gmail.com>
2018-03-15 22:16:15 +0200

    conflict fix

--- src/main/kotlin/lt/tlistas/loginn/backend/Beans.kt
+++ src/main/kotlin/lt/tlistas/loginn/backend/Beans.kt
@@ -1,7 +1,20 @@
 package lt.tlistas.loginn.backend
 
+import com.mongodb.MongoClient
+import com.mongodb.MongoCredential
+import com.mongodb.ServerAddress
+import com.mongodb.WriteConcern
 import lt.tlistas.loginn.backend.route.CollaboratorRoutes
 import lt.tlistas.loginn.backend.route.WorkLogRoutes
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
+import org.springframework.context.support.beans
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.reactive.function.server.RouterFunctions
 import java.util.*
 
 fun beans() = beans {
--- src/main/kotlin/lt/tlistas/loginn/backend/route/WorkLogRoutes.kt
+++ src/main/kotlin/lt/tlistas/loginn/backend/route/WorkLogRoutes.kt
@@ -2,6 +2,12 @@ package lt.tlistas.loginn.backend.route
 
 import lt.tlistas.loginn.backend.handler.identityconfirmed.WorkLogHandler
 import org.aspectj.apache.bcel.classfile.AttributeUtils.accept
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.web.reactive.function.BodyInserters.fromObject
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.router
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.router
 
 class WorkLogRoutes(private val workLogHandler: WorkLogHandler) {
 
