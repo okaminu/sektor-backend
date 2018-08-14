b50807dd3ab4378d8d6f34eea8cbdba079f275a9
Andrius Degutis <andriusdgt@gmail.com>
2018-08-14 19:22:57 +0300

    BA-659: prototyping worklog description and duration serving without tests

--- src/main/kotlin/lt/boldadmin/sektor/backend/handler/identityconfirmed/WorkLogHandler.kt
+++ src/main/kotlin/lt/boldadmin/sektor/backend/handler/identityconfirmed/WorkLogHandler.kt
@@ -5,10 +5,8 @@ import lt.boldadmin.nexus.service.location.LocationWorkLogService
 import lt.boldadmin.nexus.service.worklog.WorkLogService
 import lt.boldadmin.sektor.backend.service.CollaboratorAuthenticationService
 import org.springframework.web.reactive.function.BodyInserters
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.ServerResponse.ok
-import org.springframework.web.reactive.function.server.bodyToMono
 import reactor.core.publisher.Mono
 
 open class WorkLogHandler(
@@ -21,6 +19,21 @@ open class WorkLogHandler(
             .doOnNext { locationWorkLogService.logWork(collaboratorAuthService.getCollaborator(req), it) }
             .flatMap { ok().build() }
 
+    open fun getIntervalEndpoints(req: ServerRequest) =
+        ok().body(
+            Mono.just(
+                mapOf(
+                    "workLogs" to workLogService.getIntervalEndpoints(req.pathVariable("intervalId")),
+                    "workDuration" to workLogService.measureDuration(req.pathVariable("intervalId"))
+                )
+            )
+        )
+
+    open fun getIntervalIdsByCollaborator(req: ServerRequest) =
+        ok().body(
+            Mono.just(getWorkLogIntervalIdsByCollaboratorId(req))
+        )
+
     open fun getProjectNameOfStartedWork(req: ServerRequest): Mono<ServerResponse> =
         ok().body(
             BodyInserters.fromObject(
@@ -28,15 +41,33 @@ open class WorkLogHandler(
             )
         )
 
+    open fun getDescription(req: ServerRequest) =
+        ok().body(
+            Mono.just(workLogService.getDescription(req.pathVariable("intervalId")))
+        )
+
+    open fun getDurationsSum(req: ServerRequest): Mono<ServerResponse> {
+        return ok().body(
+            Mono.just(workLogService.sumWorkDurations(req.pathVariable("intervalIds").split(",")))
+        )
+    }
+
     open fun updateDescription(req: ServerRequest): Mono<ServerResponse> =
         req.bodyToMono<String>()
-            .doOnNext { workLogService.updateDescription(req.pathVariable("intervalId"), it)}
+            .doOnNext { workLogService.updateDescription(req.pathVariable("intervalId"), it) }
             .flatMap { ok().build() }
 
     open fun hasWorkStarted(req: ServerRequest): Mono<ServerResponse> =
         ok().body(
-                BodyInserters.fromObject(
-                        workLogService.hasWorkStarted(collaboratorAuthService.getCollaboratorId(req))
-                )
+            BodyInserters.fromObject(
+                workLogService.hasWorkStarted(collaboratorAuthService.getCollaboratorId(req))
+            )
         )
+
+    private fun getWorkLogIntervalIdsByCollaboratorId(req: ServerRequest): List<String> {
+        return workLogService.getByCollaboratorId(collaboratorAuthService.getCollaboratorId(req))
+            .map { it.intervalId }
+            .distinct()
+            .toList()
+    }
 }
--- src/main/kotlin/lt/boldadmin/sektor/backend/route/WorkLogRoutes.kt
+++ src/main/kotlin/lt/boldadmin/sektor/backend/route/WorkLogRoutes.kt
@@ -1,10 +1,10 @@
 package lt.boldadmin.sektor.backend.route
 
 import lt.boldadmin.sektor.backend.handler.identityconfirmed.WorkLogHandler
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.router
-import org.springframework.http.MediaType.APPLICATION_JSON
 
 class WorkLogRoutes(private val workLogHandler: WorkLogHandler) {
 
@@ -15,8 +15,12 @@ class WorkLogRoutes(private val workLogHandler: WorkLogHandler) {
                 POST("/update-description/{intervalId}", workLogHandler::updateDescription)
                 GET("/project-name-of-started-work", workLogHandler::getProjectNameOfStartedWork)
                 GET("/has-work-started", workLogHandler::hasWorkStarted)
+                GET("/interval/{intervalId}/endpoints", workLogHandler::getIntervalEndpoints)
+                GET("/interval/{intervalId}/description", workLogHandler::getDescription)
+                GET("/interval/{intervalIds}/durations-sum", workLogHandler::getDurationsSum)
+                GET("/collaborator/interval-ids", workLogHandler::getIntervalIdsByCollaborator)
             }
         }
-        GET("/status", { ok().body(fromObject("OK")) })
+        GET("/status") { ok().body(fromObject("OK")) }
     }
 }
