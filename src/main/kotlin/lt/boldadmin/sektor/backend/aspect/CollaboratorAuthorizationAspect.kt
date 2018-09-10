309d0f03a613fa68688393e5d700432923a1e8c8
Andrius Degutis <andriusdgt@gmail.com>
2018-09-10 22:15:44 +0300

    BA-443: pointcut update to match more methods, which accept client interval id

--- src/main/kotlin/lt/boldadmin/sektor/backend/aspect/CollaboratorAuthorizationAspect.kt
+++ src/main/kotlin/lt/boldadmin/sektor/backend/aspect/CollaboratorAuthorizationAspect.kt
@@ -5,6 +5,7 @@ import lt.boldadmin.sektor.backend.exception.WorkLogIntervalDoesNotBelongToColla
 import lt.boldadmin.sektor.backend.service.CollaboratorAuthenticationService
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
+import org.aspectj.lang.annotation.Pointcut
 import org.springframework.core.annotation.Order
 import org.springframework.web.reactive.function.server.ServerRequest
 
@@ -14,8 +15,21 @@ class CollaboratorAuthorizationAspect(
     private val workLogService: WorkLogService,
     private val collaboratorAuthService: CollaboratorAuthenticationService) {
 
-    @Before("execution(* lt.boldadmin.sektor.backend.handler.identityconfirmed.WorkLogHandler.updateDescription(..))" +
+    @Pointcut("execution(* lt.boldadmin.sektor.backend.handler.identityconfirmed.WorkLogHandler.updateDescription(..))" +
         " && args(req)")
+    private fun updatesWorkLogIntervalDescription(req: ServerRequest) {}
+
+    @Pointcut("execution(* lt.boldadmin.sektor.backend.handler.identityconfirmed.WorkLogHandler.getIntervalEndpoints(..))" +
+        " && args(req)")
+    private fun requestsWorkLogIntervalEndpoints(req: ServerRequest) {}
+
+    @Pointcut("execution(* lt.boldadmin.sektor.backend.handler.identityconfirmed.WorkLogHandler.getDescription(..))" +
+        " && args(req)")
+    private fun requestsWorkLogIntervalDescription(req: ServerRequest) {}
+
+    @Before("updatesWorkLogIntervalDescription(req)" +
+        " || requestsWorkLogIntervalEndpoints(req)" +
+        " || requestsWorkLogIntervalDescription(req)")
     fun collaboratorHasWorkLogIntervalAdvice(req: ServerRequest) {
         val collaboratorId = collaboratorAuthService.getCollaboratorId(req)
         if (!workLogService.doesCollaboratorHaveWorkLogInterval(collaboratorId, req.pathVariable("intervalId")))
